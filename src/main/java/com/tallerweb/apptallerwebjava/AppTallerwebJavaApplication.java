8730afb0cd2af472a4fb8b58ce08ba58aa6f49c1
Maxi Alvarez <maxi25294@gmail.com>
2023-01-11 19:04:12 -0300

    Spring Boot - First Commit

---
+++ src/main/java/com/tallerweb/apptallerwebjava/AppTallerwebJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tallerweb.apptallerwebjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTallerwebJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTallerwebJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tallerweb/apptallerwebjava/AppTallerwebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallerweb.apptallerwebjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTallerwebJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
